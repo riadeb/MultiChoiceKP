b5dbd0364a821953ca3a43b89bfdcd4e47b13b80
riadeb <benbaki.riade.mp@gmail.com>
2020-01-14 23:30:51 +0100

    Finished removing LP-dominated terms

--- Reader.java
+++ Reader.java
@@ -2,12 +2,14 @@ import java.util.*;
 import  java.io.File;
 
 
+
 public class Reader {
     int p;
     int M;
     int K;
     int N;
-    ArrayList [] data;
+    String filename;
+    ArrayList<pair> [] data;
     public Reader(String filename) throws Exception {
         File file = new File(filename);
         Scanner sc = new Scanner(file);
@@ -15,6 +17,7 @@ public class Reader {
         this.M = Double.valueOf(sc.nextLine()).intValue();
         this.K = Double.valueOf(sc.nextLine()).intValue();
         this.p = Double.valueOf(sc.nextLine()).intValue();
+        this.filename = filename;
         data = new ArrayList[N];
 
         int[][][] power_values = new int[N][K][M];
@@ -50,23 +53,85 @@ public class Reader {
     }
     public static void main(String[] args) throws Exception {
         Reader rr = new Reader("testfiles-2/test4.txt");
-        rr.remmove_IP_dominated();
-        System.out.println(rr.data[0]);
+        rr.visualize_data(28," Before preprocessing");
+        rr.remove_impossible_terms();
+        rr.visualize_data(28," After removing impossible terms");
+        rr.remove_IP_dominated();
+        rr.visualize_data(28," After removing IP dominated terms");
+        rr.remove_LP_dominated();
+        rr.visualize_data(28," After removing LP dominated terms");
+
     }
-    void remmove_IP_dominated(){
+    void remove_impossible_terms() throws Exception{ //First step of pre-processing
+        pair[] mins = new pair[N]; //Array to hold the pairs with minimum power for each channel
+        int power_sum_min = 0;
+        for(int i = 0; i < N; i++){
+            pair min = Collections.min(data[i],new power_comparator());
+            mins[i] = min;
+            power_sum_min += min.p;
+        }
+        if (power_sum_min > this.p) throw new Exception("Impossible problem instance");
         for (int n = 0; n < N; n++) {
             ArrayList<pair> channel_n = data[n];
-            HashSet<pair> channel_n_hash = new HashSet<pair>(channel_n); //to remove dominated terms in constant time
-            Collections.sort(channel_n ,new power_comparator());
-            int max_r = channel_n.get(0).r;
-            for(int i = 1; i < channel_n.size(); i++) {
-                pair curr_pair = channel_n.get(i);
-                if (curr_pair.r <= max_r) channel_n_hash.remove(curr_pair);
-                max_r = Math.max(max_r,curr_pair.r);
+            HashSet<pair> channel_n_hash = new HashSet<pair>(channel_n);
+            for (pair pai:channel_n) {
+                if (power_sum_min + pai.p - mins[n].p > this.p) channel_n_hash.remove(p);
             }
             data[n] = new ArrayList(channel_n_hash);
         }
 
+    }
+    void remove_IP_dominated() {
+        for (int n = 0; n < N; n++) {
+            ArrayList<pair> channel_n = data[n];
+            HashSet<pair> channel_n_hash = new HashSet<pair>(channel_n); //to remove dominated terms in constant time
+            Collections.sort(channel_n, new power_comparator());
+            int max_r = channel_n.get(0).r;
+            for (int i = 1; i < channel_n.size(); i++) {
+                pair curr_pair = channel_n.get(i);
+                if (curr_pair.r <= max_r) channel_n_hash.remove(curr_pair);
+                max_r = Math.max(max_r, curr_pair.r);
+            }
+            data[n] = new ArrayList(channel_n_hash);
+        }
+
+    }
+    boolean is_point_right(pair p1,pair p2,pair p3){ //true if p1 is on the right of the line p3 -> p2
+
+            return (p2.r - p3.r)*(p1.p - p2.p)>= (p1.r - p2.r)*(p2.p - p3.p) ;
+
+
+    }
+    void remove_LP_dominated(){
+        for (int n = 0; n < N; n++) {
+            ArrayList<pair> channel_n = data[n];
+            Collections.sort(channel_n ,new power_comparator());
+            Stack<pair> upper_convex_hull = new Stack<pair>();
+            upper_convex_hull.push(channel_n.get(0));
+            for(int i=1; i < channel_n.size(); i++){
+                pair p1 = channel_n.get(i);
+                if (upper_convex_hull.size() > 1) {
+                    pair p2 = upper_convex_hull.pop();
+                    pair p3 = upper_convex_hull.peek();
+                    while(!is_point_right(p1,p2,p3) && upper_convex_hull.size() > 1) {
+                        p2 = upper_convex_hull.pop();
+                        p3 = upper_convex_hull.peek();
+
+
+                    }
+                    if (is_point_right(p1,p2,p3)) upper_convex_hull.push(p2);
+                }
+                upper_convex_hull.push(p1);
+
+            }
+            data[n] = new ArrayList<pair>(upper_convex_hull);
+        }
+    }
+    void visualize_data(int channel,String additionnal_title){
+       data_visualiser chart = new data_visualiser("Instance scatter plot - file :" + filename + additionnal_title,data,channel);
+        chart.setSize(800, 400);
+        chart.setLocationRelativeTo(null);
+        chart.setVisible(true);
     }
 }
 class power_comparator implements Comparator{
@@ -76,8 +141,8 @@ class power_comparator implements Comparator{
         if (pair1.p > pair2.p) return 1;
         else if(pair1.p < pair2.p) return -1;
         else {
-            if (pair1.r > pair2.r) return 1;
-            else if (pair1.r < pair2.r) return -1;
+            if (pair1.r > pair2.r) return -1;
+            else if (pair1.r < pair2.r) return 1;
             else return 0;
         }
 
@@ -102,5 +167,9 @@ class pair {
         pair newp = (pair) obj;
         return (user == newp.user && m == newp.m);
 
+    }
+    @Override
+    public String toString(){
+        return "("+String.valueOf(this.p) + ","+String.valueOf(this.r)+")";
     }
 }
---
+++ data_visualiser.java
@@ -0,0 +1,33 @@
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class data_visualiser extends JFrame {
+    public data_visualiser(String title, ArrayList<pair> [] data, int channel){
+        super(title);
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        XYSeries series = new XYSeries("channel");
+        for (pair pp :data[channel]) {
+            series.add(pp.p, pp.r);
+
+        }
+        dataset.addSeries(series);
+
+        JFreeChart chart = ChartFactory.createScatterPlot(
+                "Channel "+ String.valueOf(channel),
+                "X-Axis",
+                "Y-Axis",
+                dataset,
+                PlotOrientation.VERTICAL,
+                true, true, false);
+        ChartPanel panel = new ChartPanel(chart);
+        setContentPane(panel);
+    }
+}
